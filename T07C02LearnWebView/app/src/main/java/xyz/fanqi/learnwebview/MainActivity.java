e92e6ec403357c81d1e8acde4f477e42cb1e2e4b
fanqi <imfanqi@qq.com>
2015-12-05 20:22:06 +0800

    WebView访问网络权限

---
+++ T07C02LearnWebView/app/src/main/java/xyz/fanqi/learnwebview/MainActivity.java
@@ -0,0 +1,19 @@
+package xyz.fanqi.learnwebview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.wv);
+        webView.loadUrl("http://www.jikexueyuan.com");
+    }
+}
