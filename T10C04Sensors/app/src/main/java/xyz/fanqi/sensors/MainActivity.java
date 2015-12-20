8a3e573813a6a101fbbe8bba0a3b8a1c42157a0b
fanqi <imfanqi@qq.com>
2015-12-20 15:05:25 +0800

    使用传感器，距离传感器，加速度传感器，方向传感器

--- T10C04Sensors/app/src/main/java/xyz/fanqi/sensors/MainActivity.java
+++ T10C04Sensors/app/src/main/java/xyz/fanqi/sensors/MainActivity.java
@@ -12,6 +12,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
+    private Sensor sensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +27,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 //        for (Sensor sensor : sensorList) {
 //            System.out.println(sensor.getName());
 //        }
-        //距离传感器,一般在手机上方
-        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-        sensorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        switch (event.sensor.getType()){
+        switch (event.sensor.getType()) {
             case Sensor.TYPE_PROXIMITY:
                 System.out.println(event.values[0]);//通过输出的数据可以判断前方是否有障碍物
+                break;
+            case Sensor.TYPE_ACCELEROMETER://可以获取三个值,xyz三个方向的
+                System.out.format("x:%f,y:%f,z:%f\n", event.values[0], event.values[1], event.values[2]);
+                break;
+            case Sensor.TYPE_ORIENTATION:
+                System.out.format("%f\n",event.values[0]);
                 break;
         }
     }
@@ -43,5 +48,29 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //距离传感器,一般在手机上方
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        //sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        //加速度传感器
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        //指南针传感器
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //可以注销所有的传感器监听器,也可以注销特定的传感器监听器
+        sensorManager.unregisterListener(this);
     }
 }
