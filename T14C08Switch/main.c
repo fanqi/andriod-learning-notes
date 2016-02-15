09c47f27da23ec7c6ef2f23fc1d39d905205c093
fanqi <imfanqi@qq.com>
2016-02-15 20:42:35 +0800

    C 语言中的流程控制与循环:

---
+++ T14C08Switch/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define UP 1
+#define DOWN 2
+#define LEFT 3
+#define RIGHT 4
+
+int main() {
+    int32_t dir = 5;
+    switch (dir) {
+        case UP:
+            printf("Go Up\n");
+            break;
+        case DOWN:
+            printf("Go Down\n");
+            break;
+        case LEFT:
+            printf("Go Left\n");
+            break;
+        case RIGHT:
+            printf("Go Right\n");
+            break;
+        default://其他情况
+            printf("Dir Unkown\n");
+    }
+    return 0;
+}
