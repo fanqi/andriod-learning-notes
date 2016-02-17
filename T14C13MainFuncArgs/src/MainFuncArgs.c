9e16699481de549241e13374a8addea47f08d234
fanqi <imfanqi@qq.com>
2016-02-17 22:46:53 +0800

    C语言函数：main函数及返回值

--- T14C13MainFuncArgs/src/MainFuncArgs.c
+++ T14C13MainFuncArgs/src/MainFuncArgs.c
@@ -11,6 +11,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * 获取程序参数
+ *
+ * */
 int main(int argc, char **argv) {
 
 	printf("args count is %d\n", argc);
@@ -25,12 +29,27 @@ int main(int argc, char **argv) {
 		printf("arg index:%d,value i:%s\n", i, argv[i]);
 	}
 	/*
-	 * cd /Users/fanqi/Documents/cdt/T14C13MainFuncArgs/Debug
 	 * ./T14C13MainFuncArgs a b c
 	 * arg index:0,value i:./T14C13MainFuncArgs
 	 * arg index:1,value i:a
 	 * arg index:2,value i:b
 	 * arg index:3,value i:c
+	 *
+	 * ./T14C13MainFuncArgs "a b" c
+	 * arg index:0,value i:./T14C13MainFuncArgs
+	 * arg index:1,value i:a b
+	 * arg index:2,value i:c
+	 *
 	 */
-	return EXIT_SUCCESS;
+
+	/*
+	 * 程序返回值
+	 * 默认定义了以下两种：
+	 * 正常退出 EXIT_SUCCESS	0
+	 * 异常退出 EXIT_FAILURE	1
+	 *
+	 * 也可自定义返回值，例如通过守护进程实现程序异常退出状态的提示
+	 *
+	 */
+	return EXIT_FAILURE;
 }
