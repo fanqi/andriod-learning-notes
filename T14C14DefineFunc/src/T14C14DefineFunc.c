d2bd806cce42dc2ae9f578c8225c3c6796c67fc9
fanqi <imfanqi@qq.com>
2016-02-20 10:51:20 +0800

    C语言常用预处理：宏函数参数连接

--- T14C14DefineFunc/src/T14C14DefineFunc.c
+++ T14C14DefineFunc/src/T14C14DefineFunc.c
@@ -23,12 +23,26 @@ int _max(int a,int b){
 	return a>b?a:b;
 }
 
+//宏函数参数连接
+void sayHi(){
+	printf("hi c\n");
+}
+
+void sayHello(){
+	printf("hello c\n");
+}
+
+#define call(NAME) say##NAME()
+
 int main(void) {
 	printf("max number is %d\n", MAX(2, 5));
 
 	LOOP(2, 100,
 			printf("current index is %d\n", index);
 	)
+
+	call(Hi);
+	call(Hello);
 
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
